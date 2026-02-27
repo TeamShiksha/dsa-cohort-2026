3e421b025f2c04bd69fcff644ad73aa0148cb25a
Kumar Nirupam <kumar.nirupam24@gmail.com>
2026-02-27 17:20:09 +0530

    Added solution for longest sub array with sum in Java

---
+++ sessions/2026-01-28-session20/solutions/java/longest_sub_array_with_sum_Kumar Nirupam.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        // code here
+        int prefixSum =0;
+        int res =0;
+        
+        HashMap<Integer , Integer > map = new HashMap<>();
+        
+        for(int i=0;i<= arr.length-1;i++){
+            
+            // continuous sum calculate
+            prefixSum = prefixSum + arr[i];
+            
+            
+            
+            // store it in map
+            if(!map.containsKey(prefixSum)){
+                map.put(prefixSum , i);
+            }
+            
+            // check if prefixSum == k
+            
+            if(prefixSum == k){
+                res = i+1;
+            }
+            
+            
+            // check if complement (prefix-k) exist in map
+            
+            if(map.containsKey((prefixSum - k))){
+                
+                res = Math.max(res,( i - map.get(prefixSum - k)));
+            }
+            
+        }
+        return res;
+    }
+}
