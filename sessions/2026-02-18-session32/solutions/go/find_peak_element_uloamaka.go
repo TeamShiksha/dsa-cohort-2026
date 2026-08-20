934951218151037bed8df824a7eacb2d5e9f02eb
Godsgift Uloamaka <godsgiftuloamaka235@gmail.com>
2026-08-20 03:23:08 +0100

    add soution to the session32 problem

---
+++ sessions/2026-02-18-session32/solutions/go/find_peak_element_uloamaka.go
@@ -0,0 +1,26 @@
+/* 
+	Probllem link: https://leetcode.com/problems/find-peak-element/
+*/
+
+package main
+
+func findPeakElement(nums []int) int {
+    left, right := 0, len(nums)-1
+
+    for left < right {
+        mid := left + (right-left)/2
+
+        if nums[mid] > nums[mid+1] {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+
+    return left
+}
+
+/* 
+	Time complexity: O(log n),
+	Space complexity: O(1)
+*/
